a63ee94728498f393f377ab79ed2de11a2fad1c3
sabareeshk <sabareeshk199@gmail.com>
2016-02-21 00:51:11 +0530

    first commit

---
+++ app/models/users.js
@@ -0,0 +1,10 @@
+//Initialize mongoose
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+module.exports = mongoose.model('user', new Schema({
+    name: String,
+    password: String,
+    admin: Boolean
+}));
---
+++ config.js
@@ -0,0 +1,6 @@
+//Initialize database settings
+
+module.exports = {
+	'secret': 'mashadinkaa',
+	'database': 'mongodb://admin:123@ds061325.mongolab.com:61325/ndash'
+}
